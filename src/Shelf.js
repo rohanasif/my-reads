5c2200d03ccf607686c9b3caba17bc9002b9087b
Rohan <rohanasif1990@gmail.com>
2022-11-12 13:45:01 +0500

    add key prop

--- src/Shelf.js
+++ src/Shelf.js
@@ -6,17 +6,25 @@ export function Shelf({ shelfTitle }) {
 
     const [shelf, setShelf] = useState([])
     const [books, setBooks] = useState([])
+    const [book, setBook] = useState([])
 
     useEffect(() => {
-        setBooks(getAll())
+
+    })
+
+    useEffect(() => {
+        getAll().then((books) => {
+            setBooks(books);
+        })
     }, [])
+
     return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">{shelfTitle}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {books.map(book => {
-                        <li><Book /></li>
+                    {books.map((book, id) => {
+                        return <li key={id}><Book /></li>
                     })}
                 </ol>
             </div>
